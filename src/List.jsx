ee8a0e6cac1181cf5fe113fc0620d3edbad02392
neetipatel11 <117623853+neetipatel11@users.noreply.github.com>
2024-02-02 09:41:47 +0530

    Add files via upload

---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function List(){
+    return (
+        <ol>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+        </ol>
+    );
+}
+export default List;
